0c50fb399b8e947389da91db5c3266d69c761331
yen.nguyen12 <yen.nguyen12@mservice.com.vn>
2025-06-28 13:35:15 +0700

    add test feature in new branch

---
+++ test-feature.py
@@ -0,0 +1,8 @@
+# Test file for Git branching
+print("Hello from feature branch!")
+
+def test_function():
+    return "This is a test function in feature branch"
+
+if __name__ == "__main__":
+    print(test_function()) 
